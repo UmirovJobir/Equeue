acbaef9cb7453c9ccbd04d2d1153ec6887dea993
UmirovJobir <jobirumirovmoliya@gmail.com>
2024-06-05 18:16:14 +0500

    delete comments in Auth

--- Account/serializers.py
+++ Account/serializers.py
@@ -125,18 +125,12 @@ class ResendConfirmationCodeSerializer(serializers.Serializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
-    # new_phone = serializers.CharField(write_only=True, required=False)
     phone = serializers.CharField(required=False)
 
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'email', 'phone'] #, 'new_phone']
+        fields = ['id', 'first_name', 'last_name', 'email', 'phone']
 
-    # def validate_new_phone(self, value):
-    #     if User.objects.filter(phone=value).exists():
-    #         raise serializers.ValidationError("This phone number is already in use.")
-    #     return value
-    
     def validate_phone(self, value):
         if self.instance and value != self.instance.phone:
             if User.objects.filter(phone=value).exists():
@@ -152,22 +146,9 @@ class UserSerializer(serializers.ModelSerializer):
         return value
 
     def update(self, instance, validated_data):
-        # new_phone = validated_data.pop('new_phone', None)
         if 'phone' in validated_data:
             validated_data.pop('phone')  # Remove phone from validated data to prevent direct update
 
-        
-        # if new_phone:
-        #     confirmation_code = '111111' if settings.DEBUG else str(random.randint(100000, 999999))
-        #     instance.confirmation_code = confirmation_code
-        #     instance.expiration_time = timezone.now() + timedelta(minutes=1, seconds=30)
-        #     instance.new_phone_temp = new_phone
-        #     instance.save()
-        #     # Send confirmation_code to the new phone number
-        #     # send_sms(new_phone, confirmation_code)
-        #     raise serializers.ValidationError({"message": "A confirmation code has been sent to the new phone number. Please confirm to complete the update."})
-
-        # Update other fields
         for attr, value in validated_data.items():
             print(attr, value)
             setattr(instance, attr, value)
