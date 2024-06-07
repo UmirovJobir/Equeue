f60c2674bae1cb440844cc6db5dd5311a857e83d
UmirovJobir <jobirumirovmoliya@gmail.com>
2024-06-07 15:07:10 +0500

    Business Delete api

--- App/signals.py
+++ App/signals.py
@@ -3,19 +3,21 @@ from django.db.models.signals import post_delete, pre_save
 from django.dispatch import receiver
 from .models import *
 
+def delete_image(image_path):
+    if os.path.isfile(image_path):
+        os.remove(image_path)
 
 @receiver(post_delete, sender=BusinessImage)
 def delete_business_image(sender, instance, **kwargs):
     if instance.image:
-        if os.path.isfile(instance.image.path):
-            os.remove(instance.image.path)
+        delete_image(instance.image.path)
 
 
 @receiver(post_delete, sender=Employee)
 def delete_employee_image(sender, instance, **kwargs):
-    if instance.image:
-        if os.path.isfile(instance.image.path):
-            os.remove(instance.image.path)
+   if instance.image:
+        delete_image(instance.image.path)
+
 
 @receiver(pre_save, sender=Employee)
 def delete_old_employee_image(sender, instance, **kwargs):
@@ -29,8 +31,7 @@ def delete_old_employee_image(sender, instance, **kwargs):
 
     old_image = old_instance.image
     if old_image and old_image != instance.image:
-        if os.path.isfile(old_image.path):
-            os.remove(old_image.path)
+        delete_image(old_image.path)
 
 
 @receiver(pre_save, sender=Business)
@@ -47,5 +48,10 @@ def delete_old_logo_on_update(sender, instance, **kwargs):
     new_logo = instance.logo
 
     if old_logo and old_logo != new_logo:
-        if os.path.isfile(old_logo.path):
-            os.remove(old_logo.path)
+        delete_image(old_logo.path)
+
+
+@receiver(post_delete, sender=Business)
+def delete_business_logo_and_images(sender, instance, **kwargs):
+    if instance.logo:
+        delete_image(instance.logo.path)
